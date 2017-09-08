60d07b0d0eeca76ba0ed7e8fc2e8a6b53ed820b1
Jake Pitkin <jakepitkin@gmail.com>
2017-09-08 11:51:04 -0600

    update

--- vis/hw2/js/Tree.js
+++ vis/hw2/js/Tree.js
@@ -20,8 +20,8 @@ class Tree {
 		this.svgWidth = 1000;
 
 		// Read in nodes from JSON file
-		for (var i = 0; i < json.length; i++) {
-			var node = new Node(json[i].name, json[i].parent);
+		for (let entry of json) {
+			let node = new Node(entry.name, entry.parent);
 			this.nodes.push(node);	
 
 			// Assign the root node of the tree
@@ -31,13 +31,14 @@ class Tree {
 		}
 
 		// Populate parentNode field
-		for (var i = 0; i < this.nodes.length; i++) {
-			var parentNode = this.nodes[i];
-			var childrenNodes = this.nodes.filter(n => n.parentName == parentNode.name);
-			for (var j = 0; j < childrenNodes.length; j++) {
-				childrenNodes[j].parentNode = parentNode;
+		for (let node of this.nodes) {
+			let parentNode = node;
+			let childrenNodes = this.nodes.filter(n => n.parentName == parentNode.name);
+			for (let childNode of childrenNodes) {
+				childNode.parentNode = parentNode;
 			}
 		}
+
 	}
 
 	/**
@@ -45,8 +46,8 @@ class Tree {
 	 */
 	buildTree() {
 		// Populate children field
-		for (var i = 0; i < this.nodes.length; i++) {
-			var parentNode = this.nodes[i];
+		for (let node of this.nodes) {
+			let parentNode = node;
 			parentNode.children = this.nodes.filter(n => n.parentName == parentNode.name);
 		}
 		// Recursively assign each node's level
@@ -60,9 +61,9 @@ class Tree {
 	 */
 	assignPosition(node, position) {
 		node.position = position;
-		for (var i = 0; i < node.children.length; i++) {
-			var position = this.determinePosition(node.children[i]);
-			this.assignPosition(node.children[i], position);
+		for (let childNode of node.children) {
+			let position = this.determinePosition(childNode);
+			this.assignPosition(childNode, position);
 		}
 	}
 
@@ -75,9 +76,9 @@ class Tree {
 			return 0;
 		}
 		// Find the greatest - parent's pos or nodes level's max pos + 1
-		var parentPos = node.parentNode.position;
-		var levelNodes = this.nodes.filter(n => n.level == node.level);
-		var levelMax = Math.max.apply(null, levelNodes.map(n => n.position));
+		let parentPos = node.parentNode.position;
+		let levelNodes = this.nodes.filter(n => n.level == node.level);
+		let levelMax = Math.max.apply(null, levelNodes.map(n => n.position));
 
 		return Math.max(parentPos, levelMax + 1);
 	}
@@ -87,8 +88,8 @@ class Tree {
 	 */
 	assignLevel(node, level) {
 		node.level = level;
-		for (var i = 0; i < node.children.length; i++) {
-			this.assignLevel(node.children[i], level + 1);
+		for (let childNode of node.children) {
+			this.assignLevel(childNode, level + 1);
 		}
 	}
 
@@ -96,7 +97,7 @@ class Tree {
 	 * Function that renders the tree
 	 */
 	renderTree() {
-		var svg = d3.select("body")
+		let svg = d3.select("body")
 					.append("svg")
 					.attr("width", this.svgWidth)
 					.attr("height", this.svgHeight);
@@ -111,12 +112,10 @@ class Tree {
 	 * @param {svg} svg - the containing svg element
 	 */
 	renderLines(svg) {
-		for (var i = 0; i < this.nodes.length; i++) {
-			var parentNode = this.nodes[i];
-			var childrenNodes = this.nodes[i].children;
-			for (var j = 0; j < childrenNodes.length; j++) {
-				var childNode = childrenNodes[j];
-				var line = svg.append("line");
+		for (let parentNode of this.nodes) {
+			let childrenNodes = parentNode.children;
+			for (let childNode of childrenNodes) {
+				let line = svg.append("line");
 				line.attr("x1", parentNode.level * this.posScalingX + this.offsetX)
 					.attr("y1", parentNode.position * this.posScalingY + this.offsetY)
 					.attr("x2", childNode.level * this.posScalingX + this.offsetX)
@@ -130,7 +129,7 @@ class Tree {
 	 * @param {svg} svg - the containing svg element 
 	 */
 	renderNodes(svg) {
-		var nodes = svg.selectAll("circle")
+		let nodes = svg.selectAll("circle")
 					   .data(this.nodes)
 					   .enter()
 					   .append("circle");
@@ -151,7 +150,7 @@ class Tree {
 	 * @param {svg} svg - the containing svg element
 	 */
 	renderLabels(svg) {
-		var nodeTexts = svg.selectAll("text")
+		let nodeTexts = svg.selectAll("text")
 						   .data(this.nodes)
 						   .enter()
 						   .append("text");
