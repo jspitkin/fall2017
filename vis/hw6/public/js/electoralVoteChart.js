50f25ec26cee680b5e86cee7c7850b8154aa179a
Jake Pitkin <jakepitkin@gmail.com>
2017-10-15 01:57:23 -0600

    vis hw6 progress

--- vis/hw6/public/js/electoralVoteChart.js
+++ vis/hw6/public/js/electoralVoteChart.js
@@ -46,110 +46,103 @@ class ElectoralVoteChart {
      * @param electionResult election data for the year selected
      * @param colorScale global quantile scale based on the winning margin between republicans and democrats
      */
+    update (electionResult, colorScale){
+        // Remove existing elements
+        this.svg.selectAll("rect").remove();
+        this.svg.selectAll("text").remove();
 
-   update (electionResult, colorScale){
-    console.log(electionResult);
-    this.svg.selectAll("rect").remove();
-    this.svg.selectAll("text").remove();
+        // Group the states based on the winning party for the state
+        let dStates = electionResult.filter(d => d['State_Winner'] == "D");
+        let rStates = electionResult.filter(d => d['State_Winner'] == "R");
+        let iStates = electionResult.filter(d => d['State_Winner'] == "I");
 
-    // Group the states based on the winning party for the state
-    let dStates = electionResult.filter(d => d['State_Winner'] == "D");
-    let rStates = electionResult.filter(d => d['State_Winner'] == "R");
-    let iStates = electionResult.filter(d => d['State_Winner'] == "I");
+        // Sort the states based on margin of victory
+        dStates.sort((a, b) => a['RD_Difference'] - b['RD_Difference']);
+        rStates.sort((a, b) => a['RD_Difference'] - b['RD_Difference']);
 
-    // Sort the states based on margin of victory
-    dStates.sort((a, b) => a['RD_Difference'] - b['RD_Difference']);
-    rStates.sort((a, b) => a['RD_Difference'] - b['RD_Difference']);
+        // Concatenate sorted states
+        let sortedElectionResult = iStates.concat(dStates).concat(rStates);
 
-    // Concatenate sorted states
-    let sortedElectionResult = iStates.concat(dStates).concat(rStates);
+        // Create a width scale
+        const D_EV_TOTAL = +electionResult[0]['D_EV_Total'];
+        const R_EV_TOTAL = +electionResult[0]['R_EV_Total'];
+        const I_EV_TOTAL = +electionResult[0]['I_EV_Total']
+        const EV_TOTAL = D_EV_TOTAL + R_EV_TOTAL + I_EV_TOTAL;
+        let widthScale = d3.scaleLinear()
+            .domain([0, EV_TOTAL])
+            .range([0, this.svgWidth]);
 
-    // Create a width scale
-    const D_EV_TOTAL = +electionResult[0]['D_EV_Total'];
-    const R_EV_TOTAL = +electionResult[0]['R_EV_Total'];
-    const I_EV_TOTAL = +electionResult[0]['I_EV_Total']
-    const EV_TOTAL = D_EV_TOTAL + R_EV_TOTAL + I_EV_TOTAL;
-    let widthScale = d3.scaleLinear()
-        .domain([0, EV_TOTAL])
-        .range([0, this.svgWidth]);
-
-    //Create the stacked bar chart
-    const BAR_Y = this.svgHeight / 4;
-    const BAR_HEIGHT = this.svgHeight / 4;
-    let currentX = 0;
-    this.svg.selectAll("rect")
-        .data(sortedElectionResult)
-        .enter()
-        .append("rect")
-        .attr("x", function(d) {
-            currentX += widthScale(d['Total_EV']);
-            return currentX - widthScale(d['Total_EV']);
-        })
-        .attr("y", BAR_Y)
-        .attr("height", BAR_HEIGHT) 
-        .attr("width", d => widthScale(d['Total_EV']))
-        .style("fill", function(d) {
-            return d['State_Winner'] == "I" ? "#01A142" : colorScale(d['RD_Difference']);
-        })
-        .classed("electoralVotes", true);
+        //Create the stacked bar chart
+        const BAR_Y = this.svgHeight / 4;
+        const BAR_HEIGHT = this.svgHeight / 4;
+        let currentX = 0;
+        this.svg.selectAll("rect")
+            .data(sortedElectionResult)
+            .enter()
+            .append("rect")
+            .attr("x", function(d) {
+                currentX += widthScale(d['Total_EV']);
+                return currentX - widthScale(d['Total_EV']);
+            })
+            .attr("y", BAR_Y)
+            .attr("height", BAR_HEIGHT) 
+            .attr("width", d => widthScale(d['Total_EV']))
+            .style("fill", function(d) {
+                return d['State_Winner'] == "I" ? "#01A142" : colorScale(d['RD_Difference']);
+            })
+            .classed("electoralVotes", true);
     
-    // Add label for independent votes (if there are any)
-    if (I_EV_TOTAL > 0) {
+        // Add label for independent votes (if there are any)
+        if (I_EV_TOTAL > 0) {
+            this.svg.append("text")
+                .attr("x", 0)
+                .attr("y", BAR_Y - 10)
+                .text(I_EV_TOTAL)
+                .classed("electoralVoteText", true)
+                .classed(this.chooseClass("I"), true);
+        }
+    
+        // Add label for dem votes
+        let firstDemXPos = this.svg.selectAll("rect")
+            .filter( (d, i) => i == iStates.length).attr("x");
         this.svg.append("text")
-            .attr("x", 0)
+            .attr("x", firstDemXPos)
             .attr("y", BAR_Y - 10)
-            .text(I_EV_TOTAL)
+            .text(D_EV_TOTAL)
             .classed("electoralVoteText", true)
-            .classed(this.chooseClass("I"), true);
-    }
-    
-    // Add label for dem votes
-    let firstDemXPos = this.svg.selectAll("rect")
-        .filter( (d, i) => i == iStates.length).attr("x");
-    this.svg.append("text")
-        .attr("x", firstDemXPos)
-        .attr("y", BAR_Y - 10)
-        .text(D_EV_TOTAL)
-        .classed("electoralVoteText", true)
-        .classed(this.chooseClass("D"), true);
+            .classed(this.chooseClass("D"), true);
    
-    // Add label for rep votes
-    let firstRepXPos = this.svg.selectAll("rect")
-        .filter( (d, i) => i == sortedElectionResult.length - 1).attr("x");
-    this.svg.append("text")
-        .attr("x", this.svgWidth)
-        .attr("y", BAR_Y - 10)
-        .text(R_EV_TOTAL)
-        .classed("electoralVoteText", true)
-        .classed(this.chooseClass("R"), true);
+        // Add label for rep votes
+        let firstRepXPos = this.svg.selectAll("rect")
+            .filter( (d, i) => i == sortedElectionResult.length - 1).attr("x");
+        this.svg.append("text")
+            .attr("x", this.svgWidth)
+            .attr("y", BAR_Y - 10)
+            .text(R_EV_TOTAL)
+            .classed("electoralVoteText", true)
+            .classed(this.chooseClass("R"), true);
 
-    // Add marker to show the middle point
-    this.svg.append("rect")
-        .attr("x", this.svgWidth / 2)
-        .attr("y", BAR_Y - 5)
-        .attr("width", 3)
-        .attr("height", BAR_HEIGHT + 10)
-        .classed("middlePoint", true);
+        // Add marker to show the middle point
+        this.svg.append("rect")
+            .attr("x", this.svgWidth / 2)
+            .attr("y", BAR_Y - 5)
+            .attr("width", 3)
+            .attr("height", BAR_HEIGHT + 10)
+            .classed("middlePoint", true);
     
-    // Add a title for the chart
-    let neededVotes = (EV_TOTAL / 2) + 1;
-    this.svg.append("text")
-        .attr("x", this.svgWidth * 0.39)
-        .attr("y", BAR_Y - 10)
-        .text("Electoral Vote (" +neededVotes + " needed to win)")
-        .classed("electoralVoteNote", true);
+        // Add a title for the chart
+        let neededVotes = (EV_TOTAL / 2) + 1;
+        this.svg.append("text")
+            .attr("x", this.svgWidth * 0.39)
+            .attr("y", BAR_Y - 10)
+            .text("Electoral Vote (" +neededVotes + " needed to win)")
+            .classed("electoralVoteNote", true);
 
-    //Just above this, display the text mentioning the total number of electoral votes required
-    // to win the elections throughout the country
-    //HINT: Use .electoralVotesNote class to style this text element
-
-    //HINT: Use the chooseClass method to style your elements based on party wherever necessary.
-
-    //******* TODO: PART V *******
-    //Implement brush on the bar chart created above.
-    //Implement a call back method to handle the brush end event.
-    //Call the update method of shiftChart and pass the data corresponding to brush selection.
-    //HINT: Use the .brush class to style the brush.
+        //******* TODO: PART V *******
+        //Implement brush on the bar chart created above.
+        //Implement a call back method to handle the brush end event.
+        //Call the update method of shiftChart and pass the data corresponding to brush selection.
+        //HINT: Use the .brush class to style the brush.
 
 
     };
--- vis/hw6/public/js/votePercentageChart.js
+++ vis/hw6/public/js/votePercentageChart.js
@@ -60,50 +60,89 @@ class VotePercentageChart {
 	 */
 	update (electionResult){
         
-	        //for reference:https://github.com/Caged/d3-tip
-	        //Use this tool tip element to handle any hover over the chart
-	        let tip = d3.tip().attr('class', 'd3-tip')
-	            .direction('s')
-	            .offset(function() {
-	                return [0,0];
-	            })
-	            .html((d)=> {
-	                /* populate data in the following format
-	                 * tooltip_data = {
-	                 * "result":[
-	                 * {"nominee": D_Nominee_prop,"votecount": D_Votes_Total,"percentage": D_PopularPercentage,"party":"D"} ,
-	                 * {"nominee": R_Nominee_prop,"votecount": R_Votes_Total,"percentage": R_PopularPercentage,"party":"R"} ,
-	                 * {"nominee": I_Nominee_prop,"votecount": I_Votes_Total,"percentage": I_PopularPercentage,"party":"I"}
-	                 * ]
-	                 * }
-	                 * pass this as an argument to the tooltip_render function then,
-	                 * return the HTML content returned from that method.
-	                 * */
-	                return;
-	            });
+		//for reference:https://github.com/Caged/d3-tip
+		//Use this tool tip element to handle any hover over the chart
+		let tip = d3.tip().attr('class', 'd3-tip')
+			.direction('s')
+			.offset(function() {
+				return [0,0];
+			})
+			.html((d)=> {
+				/* populate data in the following format
+					* tooltip_data = {
+					* "result":[
+					* {"nominee": D_Nominee_prop,"votecount": D_Votes_Total,"percentage": D_PopularPercentage,"party":"D"} ,
+					* {"nominee": R_Nominee_prop,"votecount": R_Votes_Total,"percentage": R_PopularPercentage,"party":"R"} ,
+					* {"nominee": I_Nominee_prop,"votecount": I_Votes_Total,"percentage": I_PopularPercentage,"party":"I"}
+					* ]
+					* }
+					* pass this as an argument to the tooltip_render function then,
+					* return the HTML content returned from that method.
+					* */
+				return;
+			});
+	
+		// Find the percent for each party
+		let dPercent = electionResult[0]['D_PopularPercentage'];
+		dPercent = +dPercent.substring(0, dPercent.length - 1);
+		let rPercent = electionResult[0]['R_PopularPercentage'];
+		rPercent = +rPercent.substring(0, rPercent.length - 1);
+		let iPercent = electionResult[0]['I_PopularPercentage'];
+		iPercent = +iPercent.substring(0, iPercent.length - 1);
+	
+		let widthScale = d3.scaleLinear()
+			.domain([0, 100])
+			.range([0, this.svgWidth]);
+		
+		let data = [{ 'party' : "I", 'percent' : iPercent},
+					{ 'party' : "D", 'percent' : dPercent},
+					{ 'party' : "R", 'percent' : rPercent}];
 
+		// Create the stacked bar chart
+		const BAR_Y = this.svgHeight / 4;
+		const BAR_HEIGHT = this.svgHeight / 4;
+		let currentX = 0;
+		this.svg.selectAll("rect")
+			.data(data)
+			.enter()
+			.append("rect")
+			.attr("x", function(d) {
+				currentX += widthScale(d['percent']);
+				return currentX - widthScale(d['percent']);
+			})
+			.attr("y", BAR_Y)
+			.attr("height", BAR_HEIGHT)
+			.attr("width", d => widthScale(d['percent']))
+			.attr("class", d => this.chooseClass(d['party']))
+			.classed("votesPercentage", true);
+		
+		// Add marker to show the middle point
+		this.svg.append("rect")
+			.attr("x", this.svgWidth / 2)
+			.attr("y", BAR_Y - 5)
+			.attr("width", 3)
+			.attr("height", BAR_HEIGHT + 10)
+			.classed("middlePoint", true);
+		
+		// Add a title for the chart
+		this.svg.append("text")
+			.attr("x", this.svgWidth * 0.5)
+			.attr("y", BAR_Y - 10)
+			.text("Popular Vote (50%)")
+			.classed("votesPercentageNote", true);
 
-   			  // ******* TODO: PART III *******
+		//Display the total percentage of votes won by each party
+		//on top of the corresponding groups of bars.
+		//HINT: Use the .votesPercentageText class to style your text elements;  Use this in combination with
+		// chooseClass to get a color based on the party wherever necessary
 
-		    //Create the stacked bar chart.
-		    //Use the global color scale to color code the rectangles.
-		    //HINT: Use .votesPercentage class to style your bars.
+		//Just above this, display the text mentioning details about this mark on top of this bar
+		//HINT: Use .votesPercentageNote class to style this text element
 
-		    //Display the total percentage of votes won by each party
-		    //on top of the corresponding groups of bars.
-		    //HINT: Use the .votesPercentageText class to style your text elements;  Use this in combination with
-		    // chooseClass to get a color based on the party wherever necessary
+		//Call the tool tip on hover over the bars to display stateName, count of electoral votes.
+		//then, vote percentage and number of votes won by each party.
 
-		    //Display a bar with minimal width in the center of the bar chart to indicate the 50% mark
-		    //HINT: Use .middlePoint class to style this bar.
-
-		    //Just above this, display the text mentioning details about this mark on top of this bar
-		    //HINT: Use .votesPercentageNote class to style this text element
-
-		    //Call the tool tip on hover over the bars to display stateName, count of electoral votes.
-		    //then, vote percentage and number of votes won by each party.
-
-		    //HINT: Use the chooseClass method to style your elements based on party wherever necessary.
+		//HINT: Use the chooseClass method to style your elements based on party wherever necessary.
 
 	};
 
