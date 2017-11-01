cae156da02b491b0eae2dc842ee635b4db396374
Jake Pitkin <jakepitkin@gmail.com>
2017-11-01 15:32:10 -0600

    Security PA4 done

--- security/PA4/client.py
+++ security/PA4/client.py
@@ -7,7 +7,6 @@
 from socket import *
 from Crypto import Random
 from Crypto.Hash import SHA1
-from Crypto.Cipher import AES
 from OpenSSL import crypto
 import json
 import util
@@ -25,7 +24,7 @@ def main():
     util.create_certificate_and_key(CLIENT_CERT_PATH, CLIENT_KEY_PATH)
 
     # Generate R_CLIENT and read the client's certificate from file.
-    R_CLIENT = util.generate_random_number()
+    R_CLIENT = Random.get_random_bytes(32).hex()
     print()
     print("Creating R_CLIENT.")
     print("R_CLIENT: {}".format(R_CLIENT))
@@ -36,50 +35,63 @@ def main():
     response = initial_contact(R_CLIENT, client_cert)
 
     # Extract the server's nonce.
-    R_SERVER = response['nonce']
-    server_cert = response['cert']
+    server_cert = crypto.load_certificate(crypto.FILETYPE_PEM, response['cert'])
+    cert_print = crypto.dump_certificate(crypto.FILETYPE_PEM, server_cert).decode()
+    client_priv_key = util.get_private_key(CLIENT_KEY_PATH)
+    R_SERVER = util.rsa_decrypt(bytes.fromhex(response['nonce']), client_priv_key).hex()
     print()
     print("Received initial response from the server.")
+    print("C_PUB{{R_SERVER}} {}".format(response['nonce']))
     print("R_SERVER: {}".format(R_SERVER))
+    print()
+    print("Server SSL Certificate:\n {}".format(cert_print))
     print()
 
     # Generate the master secret.
-    S = Random.get_random_bytes(32).hex()
-    print("Creating random S.")
-    print("S: {}".format(S))
-    K = util.generate_master_secret(S, R_CLIENT, R_SERVER)
-    print("Creating master key from S, R_CLIENT, and R_SERVER.")
+    K = util.generate_master_secret(R_CLIENT, R_SERVER)
+    print("Creating master key from R_CLIENT and R_SERVER.")
     print("K: {}".format(K))
     print()
 
-    # Compute the hash of the handshake messages
+
+    # Compute the hash of the handshake messages.
     handshake_messages += K
     sha1_hasher = SHA1.new()
     sha1_hasher.update(("CLIENT" + handshake_messages).encode('utf-8'))
     handshake_hash = sha1_hasher.hexdigest()
-    handshake_server(R_SERVER, K, handshake_hash)
+    S_PUB = util.get_public_key_cert(server_cert)
+    handshake_server(S_PUB, R_CLIENT, K, handshake_hash)
+    
+    # Generate keys using the master secret.
+    keys = util.generate_keys(K)
 
-    # TODO: Verify certificates
-    # TODO: Generate 4 keys with K
+    # Listen for data from the server.
+    listen_for_data(keys)
 
 
-def handshake_server(R_SERVER, K, handshake_hash):
-    cipher = AES.new(bytes.fromhex(R_SERVER), AES.MODE_ECB)
-    encrypted_secret = cipher.encrypt(bytes.fromhex(K))
-    print("cipher: {}".format(encrypted_secret.hex()))
+def handshake_server(S_PUB, R_CLIENT, K, handshake_hash):
+    encrypted_secret = util.rsa_encrypt(bytes.fromhex(K), S_PUB).hex()
+    C_PUB = util.get_public_key(CLIENT_KEY_PATH)
+    encrypted_nonce = util.rsa_encrypt(bytes.fromhex(R_CLIENT), C_PUB).hex()
     request = { 'type' : "handshake",
-                'secret' : encrypted_secret.hex(),
+                'nonce' : encrypted_nonce,
+                'secret' : encrypted_secret,
                 'hash' : handshake_hash }
     sock = socket(AF_INET, SOCK_STREAM)
     sock.connect(('', SERVER_PORT))
     packet = json.dumps(request).encode('utf-8')
     sock.send(packet)
     print()
+    print("Sending server S_PUB{{R_CLIENT}}.")
+    print("S_PUB{{R_CLIENT}}: {}".format(encrypted_nonce))
+    
+    print()
     print("Sending server encrypted secret and hash.")
     print("R_BOB{{S}}: {}".format(encrypted_secret))
+    print()
     print("Client handshake hash: {}".format(handshake_hash))
     print()
-    response = sock.recv(1024)
+    response = sock.recv(32768)
     response = json.loads(response)
     sock.close()
     print("Received server handshake hash from the server.")
@@ -97,6 +109,7 @@ def handshake_server(R_SERVER, K, handshake_hash):
     print("-----------------------------------------------")
     print("HANDSHAKE COMPLETE.")
     print("-----------------------------------------------")
+    return
 
 
 def initial_contact(R_CLIENT, client_cert):
@@ -107,7 +120,6 @@ def initial_contact(R_CLIENT, client_cert):
     request = { 'type' : "initial",
                 'algorithm' : 'RSA',
                 'message' : "I want to talk to you",
-                'nonce' : R_CLIENT,
                 'cert' : cert }
     # Send initial contact message to Bob.
     sock = socket(AF_INET, SOCK_STREAM)
@@ -116,14 +128,56 @@ def initial_contact(R_CLIENT, client_cert):
     sock.send(packet)
     print()
     print("Making initial contact with the server.")
-    print("R_ALICE: {}".format(R_CLIENT))
+    print()
     print("Algorithm: RSA")
     print()
-    response = sock.recv(1024)
+    print("Client SSL Certificate:\n {}".format(cert))
+    response = sock.recv(32768)
     response = json.loads(response)
     sock.close()
     handshake_messages += json.dumps(request) + json.dumps(response)
     return response
+
+
+def listen_for_data(keys):
+    S_ENC = keys['S_ENC']
+    S_INT = keys['S_INT']
+    C_ENC = keys['C_ENC']
+    C_INT = keys['C_INT']
+
+    # Setup socket to listen for new connections.
+    lis_sock = socket(AF_INET, SOCK_STREAM)
+    lis_sock.bind(('', CLIENT_PORT))
+    lis_sock.listen(1)
+
+    seq = 1
+    buffer = ""
+    while 1:
+        # Incoming request.
+        conn_sock, addr = lis_sock.accept()
+        packet = conn_sock.recv(10000000)
+        request = json.loads(packet.decode('utf-8'))
+        encrypted_record = request['record']
+        record = util.decrypt_plaintext(S_ENC, encrypted_record)
+        msg = record[0:20000]
+        HMAC = record[20000:]
+        expected_HMAC = util.generate_HMAC(S_INT, str(seq) + request['header'] + msg)
+        print("Got SSL record {} from server.".format(seq))
+        print("Header: {}".format(request['header']))
+        print("Expected HMAC: {}".format(expected_HMAC))
+        print("Received HMAC: {}".format(HMAC))
+        print("HMAC is correct.")
+        print("Decrypting message and adding to buffer.")
+        print()
+        if expected_HMAC != HMAC:
+            raise Exception("HMAC integrity failure.")
+        # Add partial message to the buffer and update the sequence
+        buffer = buffer + bytes.fromhex(msg).decode('utf-8')
+        seq = seq + 1
+        if seq == 6:
+            util.diff(buffer)
+            return
+
 
 if __name__ == "__main__":
     main()
--- security/PA4/util.py
+++ security/PA4/util.py
@@ -8,50 +8,39 @@ import time
 import math
 import sys
 import os.path
+import random as r
 from Crypto import Random
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 from OpenSSL import crypto
+from Crypto.Hash import SHA256
+from Crypto.Hash import HMAC
+from Crypto.Cipher import DES3
+from Crypto.Util import Padding
 
 
-def generate_random_number():
-    """ Generates a 32-byte random number where
-        the first four bytes are the Unix time.
-
-        Return:
-            nonce (string) - hexadecimal format.
-    """
-    unix_time = (math.floor(time.time())).to_bytes(32, byteorder='big')
-    unix_time_four_bytes = unix_time[28:]
-    nonce = Random.get_random_bytes(28)
-    nonce = unix_time_four_bytes + nonce
-    return nonce.hex()
-
-
-def generate_master_secret(S, n1, n2):
+def generate_master_secret(n1, n2):
     """ Generates a 32-byte master secret by performing an xor
         function on the client and server's nonces.abs
 
         Args:
-            S (string) - pre-master secret in hexadecimal.
             n1 (string) - client's nonce in hexadecimal.
             n2 (string) - server's nonce in hexadecimal.
         
         Return:
             master_secret (hex) - xor'ed shared secret.
     """
-    S_bytes = bytes.fromhex(S)
     n1_bytes = bytes.fromhex(n1)
     n2_bytes = bytes.fromhex(n2)
     xor_bytes = []
     for b1, b2 in zip(n1_bytes, n2_bytes):
         xor_bytes.append(bytes([b1 ^ b2]))
-    xor_bytes = b''.join(xor_bytes)
-    master_secret = []
-    for S_byte, xor_byte in zip(S_bytes, xor_bytes):
-        master_secret.append(bytes([S_byte ^ xor_byte]))
-    return (b''.join(master_secret)).hex()
+    return (b''.join(xor_bytes)).hex()
 
 
 def create_certificate_and_key(cert_path, key_path):
+    print()
+    print("Creating SSL certificate and key set.")
     # If the certificate and key pair already exist, do nothing.
     if os.path.isfile(cert_path) and os.path.isfile(key_path):
         return
@@ -97,3 +86,151 @@ def read_key(key_path):
         k = f.read()
     key = crypto.load_privatekey(crypto.FILETYPE_PEM, k)
     return key
+
+
+def get_public_key(key_path):
+    keys = read_key(key_path)
+    public_key = crypto.dump_publickey(crypto.FILETYPE_PEM, keys)
+    key_bytes = crypto.load_publickey(crypto.FILETYPE_PEM, public_key)
+    rsa_public_key = key_bytes.to_cryptography_key()
+    return rsa_public_key
+
+
+def get_private_key(key_path):
+    keys = read_key(key_path)
+    private_key = crypto.dump_privatekey(crypto.FILETYPE_PEM, keys)
+    key_bytes = crypto.load_privatekey(crypto.FILETYPE_PEM, private_key)
+    rsa_private_key = key_bytes.to_cryptography_key()
+    return rsa_private_key
+
+
+def get_public_key_cert(cert):
+    public_key = cert.get_pubkey()
+    pub_key = crypto.dump_publickey(crypto.FILETYPE_PEM, public_key)
+    key_bytes = crypto.load_publickey(crypto.FILETYPE_PEM, pub_key)
+    rsa_public_key = key_bytes.to_cryptography_key()
+    return rsa_public_key
+
+
+def get_padding():
+    return padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA1()), algorithm=hashes.SHA1(), label=None)
+
+
+def rsa_encrypt(plaintext, public_key):
+    return public_key.encrypt(plaintext, get_padding())
+
+
+def rsa_decrypt(ciphertext, private_key):
+    return private_key.decrypt(ciphertext, get_padding())
+
+
+def generate_50kbyte_file():
+    """ Generate a file containing 50,000 bytes. """
+    characters = ["a", "b", "c", "x", "y", "z", " "]
+    output_string = ""
+    for i in range(1000):
+        for j in range(49):
+            output_string += r.choice(characters)
+        output_string += '\n'
+    with open('test.txt', 'w') as f:
+        f.write(output_string)
+
+
+def generate_keys(K):
+    print()
+    print("Generating keys from master secret.")
+    serv_encrypt = K[0:32]
+    serv_integ = K[32:]
+    sha256_hasher = SHA256.new()
+    sha256_hasher.update(bytes.fromhex(K))
+    K = sha256_hasher.hexdigest()
+    client_encrypt = K[0:32]
+    client_integ = K[32:]
+    print("S_ENC: {}".format(serv_encrypt))
+    print("S_INT: {}".format(serv_integ))
+    print("C_ENC: {}".format(client_encrypt))
+    print("C_INT: {}".format(client_integ))
+    print()
+    keys = { 'S_ENC' : serv_encrypt,
+             'S_INT' : serv_integ,
+             'C_ENC' : client_encrypt,
+             'C_INT' : client_integ }
+    return keys
+
+
+def generate_HMAC(key, message):
+    message = message.encode('utf-8')
+    h = HMAC.new(bytes.fromhex(key), digestmod=SHA256)
+    h.update(message)
+    return h.hexdigest()
+
+
+def encrypt_plaintext(key, plaintext):
+    """ Encrypts plaintext with the given key.
+
+        Args:
+            key (hex): DES3 key.
+            plaintext (hex): unpadded plaintext.
+
+        Return:
+            ciphertext (hex): padded encrypted plaintext.
+    """
+
+    key = bytes.fromhex(key)
+    plaintext = bytes.fromhex(plaintext)
+    des3 = DES3.new(key, DES3.MODE_ECB)
+    ciphertext = des3.encrypt(Padding.pad(plaintext, DES3.block_size))
+    return ciphertext.hex()
+
+
+def decrypt_plaintext(key, ciphertext):
+    """ Decrypts a ciphertext with the given key.
+        
+        Args:
+            key (hex): DES3 key.
+            ciphertext (hex): padded ciphertext.
+        
+        Return:
+            unpadded_plaintext (hex): decrypted plaintext.
+    """
+
+    key = bytes.fromhex(key)
+    ciphertext = bytes.fromhex(ciphertext)
+    des3 = DES3.new(key, DES3.MODE_ECB)
+    plaintext = des3.decrypt(ciphertext)
+    unpadded_plaintext = Padding.unpad(plaintext, DES3.block_size)
+    return unpadded_plaintext.hex()
+
+
+def generate_SSL_data_record(enc_key, int_key, msg, seq):
+    record = ""
+    header = "application_data "
+    header += "23 "
+    header += "10k"
+    msg = msg.encode('utf-8').hex()
+    HMAC = generate_HMAC(int_key, str(seq) + header + msg)
+    cipher = encrypt_plaintext(enc_key, msg + HMAC)
+    return header, cipher, HMAC
+
+
+def diff(data):
+    expected_data = read('test.txt')
+    if expected_data != data:
+        raise Exception("Diff of received data fails with original file.")
+    print()
+    print("-----------------------------------------------")
+    print("DATA TRANSFER COMPLETE.")
+    print("-----------------------------------------------")
+    print()
+    print("Full message received from server.")
+    print("Performing diff on message . . . ")
+    print()
+    print("Diff of file is correct.")
+
+
+def read(path):
+    result = ""
+    with open(path, 'r') as f:
+        for line in f:
+            result += line
+    return result
